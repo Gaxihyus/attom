203e0f5497f2fc7809085a586c27aa5b8d21023d
Gaxyhs <47324415+Gaxihyus@users.noreply.github.com>
2023-04-15 11:45:39 -0300

    Delete events directory

--- events/ready.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'ready',
-	once: true,
-	execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}, whose in ${client.guilds.cache.size} servers, serving a total of ${client.users.cache.size} users!`);
-	},
-};
